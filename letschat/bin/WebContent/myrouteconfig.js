a8a4d67a8bbadc13a7601f668f465a7692bc4620
TM_NAYAN <35087577+NayanNIITSURAT@users.noreply.github.com>
2018-03-30 14:47:19 +0530

    backend and front end with proper Nameing convention
    AS per srinivas sir Front end and backend with proper Nameing convention

--- letschat/bin/WebContent/myrouteconfig.js
+++
@@ -1,9 +0,0 @@
-var  myApp=angular.module("myApp",["ngRoute"]);
-myApp.config(function($routeProvider) {
-	
-	console.log("i m in route provider");
-	$routeProvider.when("#/",{templateUrl:"index.jsp"})
-	.when("/register",{templateUrl:"template/registration.html"})
-	.when("/login",{templateUrl:"template/Login.html"})
-	
-})
